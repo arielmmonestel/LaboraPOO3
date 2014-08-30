444eadeca679c7a979fc94d797823037578f4764
arielmmonestel <arielmmonestel@gmail.com>
2014-08-30 10:37:21 -0600

    Avance de la l Parte
    Quede en el paso 36.

---
+++ src/aplicacion/AplCuenta.java
@@ -0,0 +1,28 @@
+package aplicacion;
+import logicaDeNegocios.Cliente;
+import logicaDeNegocios.Cuenta;
+
+public class AplCuenta {
+
+	public AplCuenta() {
+	
+	}
+
+	public static void main(String[] args) {
+	Cliente cliente1 = new Cliente("123", "Sebastian", "Rojas");
+	Cliente cliente2 = new Cliente("345", "Fiorella", "Hernadez");
+	Cuenta cuenta1 = new Cuenta(cliente1, 3000);
+	
+	Cuenta cuenta2 = new Cuenta(cliente2, 1500);
+
+	cuenta1.depositar(1500);
+	cuenta1.depositar(2000);
+	cuenta1.retirar(300);
+	cuenta2.retirar(200);
+	
+	System.out.println( cuenta1.toString() );
+	System.out.println( cuenta2.toString() );
+	
+	
+	}
+}
---
+++ src/logicaDeNegocios/Cuenta.java
@@ -0,0 +1,114 @@
+
+
+package logicaDeNegocios;
+import java.util.*;
+import java.text.*;
+
+public class Cuenta {
+
+private int numCuenta = 0;
+private Cliente duenio = null;
+private double saldo = 0;
+private static int sCantCuentas = 0;
+private Date fechaCreacion;
+private ArrayList<Operacion>operaciones;
+private int numOperaciones = 0;
+
+
+	public Cuenta(Cliente pDuenio, double pMonto ) {
+		sCantCuentas++ ;
+		setNumCuenta(sCantCuentas);
+		depositar(pMonto);
+		setDuenio(pDuenio);
+		setFechaCreacion();
+		operaciones = new ArrayList<Operacion> ();
+				
+	}
+	
+
+
+
+	public int getNumCuenta() {
+		return numCuenta;
+	}
+
+
+	public void setNumCuenta(int numCuenta) {
+		this.numCuenta = numCuenta;
+	}
+
+
+	public Cliente getDuenio() {
+		return duenio;
+	}
+
+
+	public void setDuenio(Cliente pCliente) {
+		duenio = pCliente;
+	}
+
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String depositar(double pMonto){
+		saldo +=pMonto;
+		Operacion nuevaOperacion = new Operacion (++numOperaciones, "deposito", pMonto);
+		operaciones.add(nuevaOperacion);
+		return "saldo actual despues del deposito es: " + saldo;
+	}
+
+	private boolean validarRetiro (double pMonto){
+		return pMonto <= saldo;
+	}
+
+	public String retirar (double pMonto){
+		if (validarRetiro(pMonto)){
+			saldo -= pMonto ; 
+			Operacion nuevaOperacion = new Operacion(++numOperaciones,"retiro",pMonto);
+			operaciones.add(nuevaOperacion);
+			return "El saldo actual despues del retiro es: "+ saldo;
+		}
+		else{
+			return "No tiene suficiente dinero";
+		}
+	}
+	@Override
+	public String toString() {
+		String msg;
+		msg = "Cuenta Numero: " +getNumCuenta() + "\n";
+		msg	+= "Fecha Creacion: " + getFechaCreacion() + "\n";
+		msg	+= duenio.toString() ;
+		msg	+= "Saldo: C" + getSaldo() + "\n";
+		msg += "Registro de Operaciones" + "\n";
+		for (int i = 0; i <operaciones.size();i++)
+		{	
+				Operacion unaOp = (Operacion) operaciones.get(i);
+				msg += unaOp.toString();
+		}
+		return msg;
+	}
+	
+	public void setFechaCreacion(){
+		Calendar calendario;
+		calendario = Calendar.getInstance();
+		fechaCreacion = calendario.getTime();
+		
+	}
+	
+	public String getFechaCreacion(){
+		SimpleDateFormat mascara = new SimpleDateFormat("dd/MM/yy");
+		return mascara.format(fechaCreacion);
+	}
+	
+}
+
+
+
+
